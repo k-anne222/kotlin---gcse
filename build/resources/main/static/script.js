dab104162f7d91b9ac4be9414486b038ac72b0ff
6soyoung3 <p.soyoung63@gmail.com>
2023-12-29 17:44:01 +0000

    Clear index.html

--- build/resources/main/static/script.js
+++ build/resources/main/static/script.js
@@ -1,14 +1,7 @@
 function updateMessage() {
     document.getElementById('dynamic-message').textContent = 'This message is set by JavaScript!';
 }
+
 function showWarning() { 
     alert('Warning alert!')
-}
-
-function openAbout(){
-    alert('about us page')    
-}
-
-function openContact(){
-    alert('')
 }
