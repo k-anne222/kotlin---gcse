109b8acda3074b25be6eedfbb2cf9f3f3138e5c6
6soyoung3 <p.soyoung63@gmail.com>
2024-02-23 18:23:56 +0000

    Add select level in route

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -73,6 +73,13 @@ fun main() {
                 // If not found, sends a plain text error message.
                 } ?: call.respondText("Failed to load signup.html", ContentType.Text.Plain)
             }
+            get("/selectLevel") {
+                val htmlContent = this::class.java.classLoader.getResource("select_level.html")?.readText()
+                htmlContent?.let { content ->
+                    call.respondText(content, ContentType.Text.Html)
+                // If not found, sends a plain text error message.
+                } ?: call.respondText("Failed to load select_level.html", ContentType.Text.Plain)
+            }
         }        
     // The server is instructed to keep running, awaiting requests.
     }.start(wait = true)
